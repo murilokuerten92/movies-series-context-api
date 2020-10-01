0290fc8afe89cbc68ba9fb8c6b2ef97eedd55744
Murilo Kuerten <atendimento8@cubosis.com.br>
2020-10-01 17:37:33 -0300

    second commit

--- src/routes.js
+++ src/routes.js
@@ -4,18 +4,14 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import ResultsSearch from './pages/ResultsSearch';
-//import Profile from './pages/Profile';
-//import Register from './pages/Register';
-//import NewIncident from './pages/NewIncident';
+
 
 export default function Routes() {
     return (
         <BrowserRouter>
             <Switch>
                 <Route path='/' exact component={ResultsSearch} />
-               {/* <Route path='/register' component={Register} />
-                <Route path='/profile' component={Profile} />
-    <Route path='/incidents/new' component={NewIncident} /> */}
+
             </Switch>
         </BrowserRouter>
     )
